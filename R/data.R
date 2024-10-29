93f77e28624e06f94456922e89681d5569503790
Amalie-sn <s204643@dtu.dk>
2024-10-29 09:47:41 +0100

    The captain has done a great job. go

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Title
+#'wow
+#' Description
+#'Wow look at this package. It is wild. replicating the central dogma.
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
