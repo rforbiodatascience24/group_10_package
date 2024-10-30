fa874b99e0b5d0fcc57f70a033961f0528d61916
Amalie-sn <s204643@dtu.dk>
2024-10-30 11:44:06 +0100

    protein changes to AA.

--- R/protein_plot.R
+++ R/protein_plot.R
@@ -1,9 +1,9 @@
 #' Plot proteins from sequence
 #'
-#' @param protein_seq Is the input protein sequence
-#' @param unique_protein Finds all unique proteins present in the protein sequence
-#' @param protein_plot Is the final plot of the proteins called in the global environment
-#' @param visualized_protein Is plot of the proteins
+#' @param protein_seq Is the input Aminoacid sequence
+#' @param unique_protein Finds all unique Aminoacids present in the sequence
+#' @param protein_plot Is the final plot of the Aminoacids are called in the global environment
+#' @param visualized_protein Is plot of the counts of different Aminoacids.
 #'
 #'
 #' @return the function returns a plot of how many unique protein there are present in the seq.
@@ -15,21 +15,21 @@
 #'
 #' @examples protein_plot("AGAAK") #should plot the 3 A's 1 G and 1 K
 
-# The dna sequence is split into charaters and all unique proteins are found
+# The dna sequence is split into charaters and all unique AA's are found
 protein_plot <- function(protein_seq){
   unique_protein <- protein_seq |>
     stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
     as.character() |>
     unique()
 
-# All the different proteins are counted in categories of the unique protein.
+# All the different AA's are counted in categories of the unique AA's.
   counts <- sapply(unique_protein, function(amino_acid) stringr::str_count(string = protein_seq, pattern =  amino_acid)) |>
     as.data.frame()
 
   colnames(counts) <- c("Counts")
   counts[["protein_seq"]] <- rownames(counts)
 
-# The amounts of unique proteins are plotted in a column plot
+# The amounts of unique AA's are plotted in a column plot
   visualized_protein <- counts |>
     ggplot2::ggplot(ggplot2::aes(x = protein_seq, y = Counts, fill = protein_seq)) +
     ggplot2::geom_col() +
